5e184f204d8f3a5b7f4dde1da208f7d6796b1ae9
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-02-22 21:35:36 +0100

    new data

--- figures/local-benchmark/process_data.sh
+++ figures/local-benchmark/process_data.sh
@@ -5,9 +5,9 @@ awk 'BEGIN { FS="," }
       sum_rs += $2;
       sum_tot += $3;
       sum_tot_2 += $3 * $3
-    } (NR%5) == 0 {
-        avg = sum_tot/5;
-        print sum_cp/5, sum_rs/5, avg, sum_tot_2/5 - avg * avg;
+    } (NR%10) == 0 {
+        avg = sum_tot/10;
+        print sum_cp/10, sum_rs/10, avg, sum_tot_2/10 - avg * avg;
         sum_cp = 0;
         sum_rs = 0;
         sum_tot = 0;
