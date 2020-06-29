f3fb36d131230e96954e6e3a36fba8a0b2f2a739
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-06-29 08:44:35 +0200

    wip

---
+++ thesis/slides/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm \ 
+  main_EN.aux \
+  main_EN.bbl \
+  main_EN.blg \
+  main_EN.bcf \
+  main_EN.log \
+  main_EN.nav \
+  main_EN.out \
+  main_EN.run.xml \
+  main_EN.snm \
+  main_EN.toc \
+  main_EN.pdf 
